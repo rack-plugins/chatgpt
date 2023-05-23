4a69026adc55bf8872476ee704d689569cf182af
fimreal <6913651@qq.com>
2023-05-23 16:01:52 +0800

    update help route

--- route.go
+++ route.go
@@ -9,11 +9,10 @@ func AddRoute(g *gin.Engine) {
 	if !viper.GetBool(ID) && !viper.GetBool("allservices") {
 		return
 	}
-	r := g.Group(RoutePrefix)
-
 	// list route
-	r.GET("/help/"+ID, help)
+	g.GET("/help/"+ID, help)
 
+	r := g.Group(RoutePrefix)
 	r.GET("/", Ask)
 	r.POST("/", Ask)
 }
